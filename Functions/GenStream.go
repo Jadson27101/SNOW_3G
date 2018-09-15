c1d26490796a2e05cf308815b964fc70540a9f6f
jadson27101 <jadson27101@gmail.com>
2018-09-15 16:43:33 +0300

    Correcting code

--- Functions/GenStream.go
+++ Functions/GenStream.go
@@ -1,20 +1,19 @@
 package Functions
 
 func Next(){
-	var s00 = (sState[0] >> (24)) & 0xFF
-	var s01 = (sState[0] >> (16)) & 0xFF
-	var s02 = (sState[0] >> 8) & 0xFF
-	var s03 =  (sState[0] & 0xFF)
-	var s110 = (sState[11] >> (24)) & 0xFF
-	var s111 = (sState[11] >> (16)) & 0xFF
-	var s112 = (sState[11] >> 8) & 0xFF
-	var s113 =  (sState[11] & 0xFF)
-	var v =  (((s01 << 24) | (s02 << 16) | (s03 << 8) | 0x00) ^
+	s00 := (sState[0] >> (24)) & 0xFF
+	s01 := (sState[0] >> (16)) & 0xFF
+	s02 := (sState[0] >> 8) & 0xFF
+	s03 :=  (sState[0] & 0xFF)
+	s110 := (sState[11] >> (24)) & 0xFF
+	s111 := (sState[11] >> (16)) & 0xFF
+	s112 := (sState[11] >> 8) & 0xFF
+	s113 :=  (sState[11] & 0xFF)
+	v :=  (((s01 << 24) | (s02 << 16) | (s03 << 8) | 0x00) ^
 		uint32(mul(uint8(s00))) ^ sState[2] ^ ((0x00 << 24) | (s110 << 16) | (s111 << 8) | s112) ^
 		uint32(div(uint8(s113))));
 	for i:=0; i<15; i++{
 		sState[i] = sState[i+1]
 	}
 	sState[15] = v
-	//fmt.Print(sState)
 }
